a4ac37e6902b4c95b97df99f8917f8e424bd62ef
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-06-03 11:20:18 -0700

    Damn... semicolons;

--- js/faster-math-floor.js
+++ js/faster-math-floor.js
@@ -16,8 +16,8 @@
  */
 
 (function () {
-    console.log(~(5.5))     // => -6
-    console.log(~(-6))      // => 5
-    console.log(~~5.5)      // => 5  (same as Math.floor(5.5))
-    console.log(~~(-5.5))   // => -5 (NOT the same as Math.floor(-5.5), which would give -6 )
+  console.log(~(5.5));     // => -6
+  console.log(~(-6));    // => 5
+  console.log(~~5.5);      // => 5  (same as Math.floor(5.5))
+  console.log(~~(-5.5));   // => -5 (NOT the same as Math.floor(-5.5), which would give -6 )
 })();
